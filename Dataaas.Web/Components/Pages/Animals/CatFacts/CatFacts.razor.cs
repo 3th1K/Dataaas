92dce804c0b479f621b63dbd6e487f429c0cef23
Mehedi Rahaman <mehedirahaman28@gmail.com>
2024-12-03 10:26:15 +0530

    cat fact ui update

--- Dataaas.Web/Components/Pages/Animals/CatFacts/CatFacts.razor.cs
+++ Dataaas.Web/Components/Pages/Animals/CatFacts/CatFacts.razor.cs
@@ -1,5 +1,19 @@
-﻿namespace Dataaas.Web.Components.Pages.Animals.CatFacts;
+﻿using Dataaas.Web.Models.Animals.CatFacts;
+using Dataaas.Web.Services.Animals.CatFacts;
+using Microsoft.AspNetCore.Components;
+
+namespace Dataaas.Web.Components.Pages.Animals.CatFacts;
 
 public sealed partial class CatFacts
 {
+    [Inject]
+    private ICatFactsService catFactsService { get; set; } = null!;
+
+    public List<FactModel> Facts { get; private set; } = [];
+
+    public async Task GetAsync()
+    {
+        var newFacts = await catFactsService.GetRandomFactsAsync(5);
+        Facts = newFacts;
+    }
 }
